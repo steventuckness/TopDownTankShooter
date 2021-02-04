a3ffe12f175ca9a55a7d183254859255316b30bb
Steven Tuckness <stuckne1@gmail.com>
2021-02-03 22:58:38 -0600

    add slow down on terrain, add smoke effect

--- tanks/tank.gd
+++ tanks/tank.gd
@@ -10,6 +10,7 @@ export (int) var max_speed
 export (float) var rotation_speed
 export (float) var gun_cooldown
 export (int) var max_health
+export (float) var offroad_friction
 
 export (int) var gun_shots = 1
 export (float, 0, 1.5) var gun_spread
@@ -20,9 +21,11 @@ var velocity = Vector2()
 var can_shoot = true
 var alive = true
 var health
+var map
 
 func _ready():
 	health = max_health
+	$Smoke.emitting = false
 	emit_signal('health_changed', health * 100/max_health)
 	emit_signal('ammo_changed', ammo * 100/max_ammo)
 	$GunTimer.wait_time = gun_cooldown
@@ -48,16 +51,24 @@ func _physics_process(delta):
 	if not alive:
 		return
 	control(delta)
+	if map:
+		var tile = map.get_cellv(map.world_to_map(position))
+		if tile in GLOBALS.slow_terrain:
+			velocity *= offroad_friction
 	move_and_slide(velocity)
 	
 func take_damage(amount):
 	health -= amount
+	if health < max_health / 2:
+		$Smoke.emitting = true
 	emit_signal('health_changed', health * 100/max_health)
 	if health <= 0:
 		explode()
 		
 func heal(amount):
 	health += amount
+	if health > max_health / 2:
+		$Smoke.emitting = false
 	health = clamp(health, 0, max_health)
 	emit_signal('health_changed', health * 100/max_health)
 	
